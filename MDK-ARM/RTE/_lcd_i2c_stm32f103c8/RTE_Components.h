794958c6c4c9099a6ccebc1bde7164c12ec30259
vhung6868 <20020671@vnu.edu.vn>
2023-06-11 21:30:02 +0700

    init project

---
+++ MDK-ARM/RTE/_lcd_i2c_stm32f103c8/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'lcd_i2c_stm32f103c8' 
+ * Target:  'lcd_i2c_stm32f103c8' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/lcd_i2c_stm32f103c8/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+lcd_i2c_stm32f103c8\startup_stm32f103xb.o: startup_stm32f103xb.s
