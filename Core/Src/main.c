d0f768c36210c3c30b48a778de905c1bd53d6e4c
vhung6868 <20020671@vnu.edu.vn>
2023-06-13 09:29:39 +0700

    intr

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -100,6 +100,7 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : PC13 */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
@@ -111,8 +112,8 @@ static void MX_GPIO_Init(void)
 	/*Configure GPIO pin : LED_Pin */
   GPIO_InitStruct.Pin = LED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Pull = GPIO_NOPULL; /* no pull-up/down */
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW; // toc do chuyen doi du lieu
   HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : BUTTON_Pin */
