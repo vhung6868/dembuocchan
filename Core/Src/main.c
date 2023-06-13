cae864912279d762f4bf7a43d70d5040c7f0275f
vhung6868 <20020671@vnu.edu.vn>
2023-06-13 08:41:56 +0700

    init en

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -26,7 +26,7 @@ uint32_t u32Count = 0U;
 #define WHO_AM_I_REG 0x75
 
 #define LED_Pin GPIO_PIN_11
-#define LED_GPIO_Port GPIOC
+#define LED_GPIO_Port GPIOA
 #define BUTTON_Pin GPIO_PIN_0
 #define BUTTON_GPIO_Port GPIOA
 
@@ -38,148 +38,6 @@ int16_t Accel_Z_RAW = 0;
 
 float Ax, Ay, Az;
 
-void MPU6050_Init (void)
-{
-	uint8_t check;
-	uint8_t Data;
-
-
-	HAL_I2C_Mem_Read (&hi2c1, MPU6050_ADDR,WHO_AM_I_REG,1, &check, 1, 1000);
-
-	if (check == 104)  // 0x68 will be returned by the sensor if everything goes well
-	{
-		// power management register 0X6B we should write all 0's to wake the sensor up
-		Data = 0;
-		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, PWR_MGMT_1_REG, 1,&Data, 1, 1000);
-
-		// Set DATA RATE of 1KHz by writing SMPLRT_DIV register
-		Data = 0x07;
-		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, SMPLRT_DIV_REG, 1, &Data, 1, 1000);
-
-		// Set accelerometer configuration in ACCEL_CONFIG Register
-		// XA_ST=0,YA_ST=0,ZA_ST=0, FS_SEL=0 -> ± 2g
-		Data = 0x00;
-		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, ACCEL_CONFIG_REG, 1, &Data, 1, 1000);
-
-		// Set Gyroscopic configuration in GYRO_CONFIG Register
-		// XG_ST=0,YG_ST=0,ZG_ST=0, FS_SEL=0 -> ± 250 °/s
-		Data = 0x00;
-		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, GYRO_CONFIG_REG, 1, &Data, 1, 1000);
-	}
-
-}
-
-
-void MPU6050_Read_Accel (void)
-{
-	uint8_t Rec_Data[6];
-
-	// Read 6 BYTES of data starting from ACCEL_XOUT_H register
-
-	HAL_I2C_Mem_Read (&hi2c1, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 6, 1000);
-
-	Accel_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data [1]);
-	Accel_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data [3]);
-	Accel_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data [5]);
-
-	Ax = Accel_X_RAW/16384.0;
-	Ay = Accel_Y_RAW/16384.0;
-	Az = Accel_Z_RAW/16384.0;
-}
-
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
- 
-	char buf[4];
-  HAL_Init();
-
-  SystemClock_Config();
- 
-  MX_GPIO_Init();
-  MX_I2C1_Init();
-  
-  lcd_init();
-
-  MPU6050_Init();
-
-  lcd_send_string ("initialized");
-
-  HAL_Delay (200);  
-
-  lcd_clear ();
-	
-float vectorprevious;
-
-float vector;
-
-float totalvector;
-
-float Steps = 0;
-
-  while (1)
-  {
-   
-			if(HAL_GPIO_ReadPin(BUTTON_GPIO_Port,BUTTON_Pin) == 0 )
-			{
-						
-				HAL_Delay(2);
-				if(HAL_GPIO_ReadPin(BUTTON_GPIO_Port,BUTTON_Pin) == 0) {
-				while(HAL_GPIO_ReadPin(BUTTON_GPIO_Port,BUTTON_Pin) == 0 ) {}
-					u32Count++;
-					
-				if( u32Count %2 == 0 )
-				{
-										
-					while(HAL_GPIO_ReadPin(BUTTON_GPIO_Port,BUTTON_Pin) == 1)
-						{
-							
-							MPU6050_Read_Accel();
-
-							vector = sqrt( (Ax * Ax) + (Ay * Ay) + (Az * Az) );
-		
-							if (totalvector > 1.08){
-
-			Steps=Steps+1;
-			vector = vectorprevious;
-						}
-							totalvector = vector - vectorprevious;
-							lcd_send_cmd (0x80|0x00);
-							lcd_send_string ("walkie=");
-							sprintf(buf,"%.0f",Steps);
-							lcd_send_string(buf);
-											
-							lcd_send_cmd (0x80|0x40);
-							lcd_send_string ("status=active");
-												
-						HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-											
-							HAL_Delay(200);
-			
-			}
-		}
-				else 
-				{
-				lcd_send_cmd (0x80|0x40);
-							lcd_send_string ("status=paused");
-					
-				}
-
-}
-}
-	
-}
-  
-}
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -270,3 +128,149 @@ static void MX_GPIO_Init(void)
 void Error_Handler(void)
 {
 }
+
+
+void MPU6050_Init (void)
+{
+	uint8_t check;
+	uint8_t Data;
+
+
+	HAL_I2C_Mem_Read (&hi2c1, MPU6050_ADDR,WHO_AM_I_REG,1, &check, 1, 1000);
+
+	if (check == 104)  // 0x68 will be returned by the sensor if everything goes well
+	{
+		// power management register 0X6B we should write all 0's to wake the sensor up
+		Data = 0;
+		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, PWR_MGMT_1_REG, 1,&Data, 1, 1000);
+
+		// Set DATA RATE of 1KHz by writing SMPLRT_DIV register
+		Data = 0x07;
+		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, SMPLRT_DIV_REG, 1, &Data, 1, 1000);
+
+		// Set accelerometer configuration in ACCEL_CONFIG Register
+		// XA_ST=0,YA_ST=0,ZA_ST=0, FS_SEL=0 -> ± 2g
+		Data = 0x00;
+		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, ACCEL_CONFIG_REG, 1, &Data, 1, 1000);
+
+		// Set Gyroscopic configuration in GYRO_CONFIG Register
+		// XG_ST=0,YG_ST=0,ZG_ST=0, FS_SEL=0 -> ± 250 °/s
+		Data = 0x00;
+		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, GYRO_CONFIG_REG, 1, &Data, 1, 1000);
+	}
+
+}
+
+
+void MPU6050_Read_Accel (void)
+{
+	uint8_t Rec_Data[6];
+
+	// Read 6 BYTES of data starting from ACCEL_XOUT_H register
+
+	HAL_I2C_Mem_Read (&hi2c1, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 6, 1000);
+
+	Accel_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data [1]);
+	Accel_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data [3]);
+	Accel_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data [5]);
+
+	Ax = Accel_X_RAW/16384.0;
+	Ay = Accel_Y_RAW/16384.0;
+	Az = Accel_Z_RAW/16384.0;
+}
+
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+ 
+	char buf[4];
+  HAL_Init();
+
+  SystemClock_Config();
+ 
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  
+  lcd_init();
+
+  MPU6050_Init();
+
+  lcd_send_string ("initialized");
+
+  HAL_Delay (200);  
+
+  lcd_clear ();
+	
+float vectorprevious;
+
+float vector;
+
+float totalvector;
+
+float Steps = 0;
+
+  while (1)
+  {
+			HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,1);
+			if(HAL_GPIO_ReadPin(BUTTON_GPIO_Port,BUTTON_Pin) == 0 )
+			{
+						
+				HAL_Delay(2);
+				if(HAL_GPIO_ReadPin(BUTTON_GPIO_Port,BUTTON_Pin) == 0) {
+				while(HAL_GPIO_ReadPin(BUTTON_GPIO_Port,BUTTON_Pin) == 0 ) {}
+					u32Count++;
+					
+				if( u32Count %2 == 0 )
+				{
+										
+					while(HAL_GPIO_ReadPin(BUTTON_GPIO_Port,BUTTON_Pin) == 1)
+						{
+							
+							MPU6050_Read_Accel();
+
+							vector = sqrt( (Ax * Ax) + (Ay * Ay) + (Az * Az) );
+		
+							if (totalvector > 1.1){
+
+			Steps=Steps+1;
+			vector = vectorprevious;
+						}
+							totalvector = vector - vectorprevious;
+							lcd_send_cmd (0x80|0x00);
+							lcd_send_string ("walkie=");
+							sprintf(buf,"%.0f",Steps);
+							lcd_send_string(buf);
+											
+							lcd_send_cmd (0x80|0x40);
+							lcd_send_string ("status=active");
+												
+						HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,status);
+						HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,0);
+											
+							HAL_Delay(200);
+			
+			}
+		}
+				else 
+				{
+				lcd_send_cmd (0x80|0x40);
+							lcd_send_string ("status=paused");
+					
+					
+				}
+
+}
+}
+	
+}
+  
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
